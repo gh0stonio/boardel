a16b32b09b988bb0d721a29843c819aef2420c43
gh0stonio <antoine.leveque.pro@gmail.com>
2020-10-31 20:57:31 +0100

    chore: initial commit with Next.js and base tools
    Initial commit of the project with Next.js, eslint, prettier, commitizen, husky etc.

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true, // Make sure eslint picks up the config at the root of the directory
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020, // Use the latest ecmascript standard
+    sourceType: 'module', // Allows using import/export statements
+    ecmaFeatures: {
+      jsx: true, // Enable JSX since we're using React
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the react version
+    },
+  },
+  env: {
+    browser: true, // Enables browser globals like window and document
+    node: true, // Enables Node.js global variables and Node.js scoping.
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
+  ],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+    'react/react-in-jsx-scope': 0,
+  },
+}
